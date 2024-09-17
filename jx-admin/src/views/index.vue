b2c5ee3dee7c3db485a329376565ba2b904e6375
laolang2016 <xiaodaima2016@163.com>
2024-09-17 16:27:36 +0800

    ✨ feat(jx-admin): 引入 element plus

--- jx-admin/src/views/index.vue
+++ jx-admin/src/views/index.vue
@@ -1,9 +1,28 @@
 <template>
     <div class="red">hello world</div>
+    <el-button type="primary" @click="sayHello">hello</el-button>
 </template>
 
+<script lang="ts">
+import { ElMessage } from 'element-plus'
+export default {
+    setup() {
+        const sayHello = () => {
+            ElMessage({
+                message: 'hello world',
+                type: 'success'
+            })
+            console.log('hello world')
+        }
+        return {
+            sayHello
+        }
+    }
+}
+</script>
+
 <style lang="scss">
-.red{
+.red {
     color: $baseColor;
 }
 </style>
